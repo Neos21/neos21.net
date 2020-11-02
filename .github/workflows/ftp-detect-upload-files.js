9e9cc872284f245182e1f323bf4b1cc3989586b7
Neos21 <neos21@gmail.com>
2020-11-03 00:15:37 +0900

    Modify

--- .github/workflows/ftp-detect-upload-files.js
+++ .github/workflows/ftp-detect-upload-files.js
@@ -4,6 +4,7 @@ const srcDir  = 'src/';
 const distDir = 'dist/';
 
 // 前 Step で JSON ファイルに書き出しておいた変更ファイル一覧を取得する
+// ファイルパスは `'src/pages/index.html'` のようにプロジェクトルートからの表記になっている
 const addedModified = require('../../temp/added_modified.json');
 const renamed       = require('../../temp/renamed.json');
 const removed       = require('../../temp/removed.json');  // 削除されたファイルがあるかどうかの確認だけ
@@ -14,18 +15,17 @@ if(removed.length) {
 
 const joined = [...addedModified, ...renamed];
 
-// src/templates/ 配下の変更がある場合は一切のアップロードを行わないこととする
+// `src/templates/` 配下の変更がある場合は一切のアップロードを行わないこととする
+// 全量アップロードが必要になり、GitHub Actions 内での全量アップは自信がないので手作業にする
 const isTemplateChanged = joined.some((file) => file.includes('src/templates/'));
 if(isTemplateChanged) {
   console.error('Templates Changed! Please Upload Manually. Aborted');
   return process.exit(1);
 }
 
-// src/pages/ 配下の変更を特定する
-const filtered = joined.filter((file) => file.includes(srcDir + 'pages/'));
-
-// ビルド後のコンテンツに対応するようファイル名を直す
-const uploadFiles = filtered.map((file) => file.replace(srcDir + 'pages/', distDir).replace('.md', '.html'));
+const uploadFiles = joined
+  .filter((file) => file.includes(srcDir + 'pages/'))  // `src/pages/` 配下の変更のみ扱う
+  .map((file) => file.replace(srcDir + 'pages/', distDir).replace('.md', '.html'));  // ファイルパス・ファイル名を直す
 
 // 変更ファイルがなければ結果ファイルを作らず終了する
 if(!uploadFiles.length) {
@@ -37,6 +37,18 @@ uploadFiles.push('dist/feeds/atom.xml');
 uploadFiles.push('dist/index.html');
 uploadFiles.push('dist/about/new.html');
 uploadFiles.push('dist/blog/index.html');
+
+// 現在年月の /blog/YYYY/MM/index.html ファイルが存在すれば、必ずアップロードする対象にする
+const now = new Date();
+const currentYearMonth = now.getFullYear() + '/' + ('0' + (now.getMonth() + 1)).slice(-2);  // `'YYYY/MM'` 形式にする
+const currentYearMonthIndex = distDir + 'blog/' + currentYearMonth + '/index.html';
+try {
+  fs.statSync(currentYearMonthIndex);  // ビルド後のファイルの存在をチェックする
+  uploadFiles.push(currentYearMonthIndex);  // 存在していればアップロード対象にする
+}
+catch(_error) {
+  console.log(`Current Year Month Index File [${currentYearMonthIndex}] Does Not Exist`);
+}
 
 const stringified = JSON.stringify(uploadFiles);
 console.log('Upload Files :');
