28f0a44dbb0552cc109e30b91da85e3fe9dacd6d
Neos21 <neos21@gmail.com>
2020-11-19 22:27:00 +0900

    Google AdSense が勘違いしやがるから一旦怪しそうなコンテンツ取り下げる

--- src/documents/neos-archives/neo-s21-design/20040611/0.js
+++
@@ -1,7 +0,0 @@
-//	document.write("<!--nobanner-->");
-//	document.write("<DIV align=\"center\">");
-//	document.write("<A HREF=\"http://img.xrea.com/ad_click.fcg?site=neo.s21.xrea.com\"><IMG SRC=\"http://img.xrea.com/ad_img.fcg?site=neo.s21.xrea.com\" HEIGHT=\"60\" WIDTH=\"468\"></A>");
-//	document.write("</DIV><!--nobanner-->");
-
-//	｢"｣を打つ時は｢\｣を先に打つ
-//	document.write("");
