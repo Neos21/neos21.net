c3ad990ad52676d89f2f4e7514d0e1c62a904b69
Neos21 <neos21@gmail.com>
2026-08-25 08:20:34 +0900

    Update

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,12 @@
 import postcssImport from 'postcss-import';
 import cssnano from 'cssnano';
 
+// PostCSS 設定
 export default {
   plugins: [
+    // `@import` の処理
     postcssImport(),
+    // 圧縮処理
     cssnano({
       preset: 'default'
     })
